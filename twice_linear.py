5d2a96cd2c9d6199db4a8e6dc1f9510f4577e8b4
rogpaxton <rogpaxton@gmail.com>
2016-12-20 07:18:53 -0700

    12202016

--- twice_linear.py
+++ twice_linear.py
@@ -2,18 +2,11 @@ def dbl_linear(n):
 
     from sets import Set
 
-    lst = [1]
+    iter_set = Set([1])
+    print n
+    for i in range(n):
+        iter_set.add(2 * min(iter_set) + 1)
+        iter_set.add(3 * min(iter_set) + 1)
+        iter_set.remove(min(iter_set))
 
-    for index, i in enumerate(range(n-1)):
-        x = lst[index]
-        lst.append(x)
-        lst.append(2 * x + 1)
-        lst.append(3 * x + 1)
-        lst = Set(lst)
-        lst = list(lst)
-        lst.sort()
-
-    print lst[n]
-    return lst[n]
-
-	# your code
+    return min(iter_set)
