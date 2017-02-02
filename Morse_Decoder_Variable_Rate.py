39e67e23661b98fed6f2636818780d3ea1e62bec
rogpaxton <rogpaxton@gmail.com>
2017-02-02 06:14:40 -0700

    02022017

---
+++ Morse_Decoder_Variable_Rate.py
@@ -0,0 +1,48 @@
+def decodeBitsAdvanced(bits):
+    # ToDo: Accept 0's and 1's, return dots, dashes and spaces
+    bits = bits.strip('0')
+    print bits
+    bits_lst = list(bits)
+
+#Approximate transmission rate
+
+    uno_max = 0
+    uno_count = 1
+    zed_max = 0
+    zed_count = 1
+
+    for index, i in enumerate(bits_lst[1:]):
+        if i == '0' and bits_lst[index] == '0':
+            zed_count += 1
+        elif i == '1' and bits_lst[index] == '1':
+            uno_count += 1
+        if uno_count > uno_max:
+            uno_max = uno_count
+        if zed_count > zed_max:
+            zed_max = zed_count
+        if i == '0' and bits_lst[index] == '1':
+            uno_count = 1
+        elif i == '1' and bits_lst[index] == '0':
+            zed_count = 1
+
+
+def decodeMorse(morseCode):
+    # ToDo: Accept dots, dashes and spaces, return human-readable message
+    word_lst = morseCode.split('       ')
+
+    dec_lst = []
+    for i in word_lst:
+        chars = []
+        char_lst = i.split('   ')
+        for j in char_lst:
+            char = j.replace(' ', '')
+            chars.append(MORSE_CODE.get(char))
+        dec_lst.append(chars)
+
+    words = []
+    for i in dec_lst:
+        word = ''.join(i)
+        words.append(word)
+
+    op = ' '.join(words)
+    return op
