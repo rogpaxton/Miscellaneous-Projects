198f7e6570b8a331e5cb0fd44399902d0af37fc9
rogpaxton <rogpaxton@gmail.com>
2017-05-10 08:12:34 -0600

    05102017

--- proper_fraction_tester.py
+++ proper_fraction_tester.py
@@ -1,16 +1,49 @@
 def proper_fractions(n):
 
-    from fractions import Fraction
+#    from fractions import Fraction
+    from fractions import gcd
+    from gc import collect
+#    import fractions
 
     count = 0
 
-    for i in range(1,n):
-        if Fraction(i, n).numerator == i:
-            count += 1
+#    evens = []
+#    odds = []
 
-#alternate method
-#    for i in range(1,n):
+#    for i in range(1, n):
+#        if i%2 == 0:
+#            evens.append(i)
+#        else:
+#            odds.append(i)
+
+#    for i in evens:
 #        if gcd(i,n) == 1:
 #            count += 1
+#    for i in odds:
+#        if gcd(i,n) == 1:
+#            count += 1
+
+#    for i in range(1,n):
+#        if Fraction(i, n).numerator == i:
+#            count += 1
+
+    for i in range(1,n):
+        check = gcd(i,n)
+        if check == 1:
+            count += 1
+        del check
+        collect()
+
+#    nums = [i for i in range(1,n) if gcd(i,n) == 1]
+#    return len(nums)
+
+#    num = 1
+
+#    while num < n:
+#        if gcd(num,n) == 1:
+#            count += 1
+#        num += 1
+
+#    mult3 = filter(lambda x: gcd(x,n) == 1, xrange(1,n))
 
     return count
